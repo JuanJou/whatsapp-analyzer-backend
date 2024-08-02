07dcf1b6d6de3cc2d1f9488b9803448bf11011b5
root <jouglardjuan@gmail.com>
2024-08-02 13:43:45 +0000

    CORS

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import metrics, users, files
+from routes import file as f
 import os
 from dotenv import load_dotenv
 
 
 app = FastAPI()
 
+origins = [
+        "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(metrics.router)
 app.include_router(users.router)
 app.include_router(files.router)
-
-@app.get("/my-first-api")
-def hello():
-  return {"Hello world!"}
+app.include_router(f.router)
